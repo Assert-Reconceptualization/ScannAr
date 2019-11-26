0aa4fa302626532372223ef72a95051600d0dec5
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-26 15:01:38 -0600

    link BusinessHeader LoginHeader

--- client/components/headers/BusinessHeader.js
+++ client/components/headers/BusinessHeader.js
@@ -1,33 +1,39 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {
+  View, Text, StyleSheet, Button,
+} from 'react-native';
 
-const BusinessHeader = ({navigator}) => {
-
+const BusinessHeader = ({ navigator }) => {
   const handleLogout = () => {
-      navigator.pop();
-  }
+    navigator.pop();
+  };
+
+  // eslint-disable-next-line no-use-before-define
+  const { titleContainer, text } = styles;
+
   return (
-    <View style={styles.titleContainer}>
-      <Text style={styles.text}>ScannAR B</Text>
-      <Button title="Logout" onPress={handleLogout}/>
+    <View style={titleContainer}>
+      <Text style={text}>ScannAR B</Text>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   titleContainer: {
-    height: "8%",
-    backgroundColor: "#505950",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "flex-end"
+    height: '8%',
+    backgroundColor: '#505950',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   text: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 30,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
 
 export default BusinessHeader;
