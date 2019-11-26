44d7c162258f3601a2437802d52f6a969a351860
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-26 11:57:25 -0600

    add business landing header

--- client/screens/BusinessLanding.js
+++ client/screens/BusinessLanding.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
-const BusinessLanding = ({ navigator }) => (
-    <View style={[styles.screen, { backgroundColor: "#23395B" }]}>
-        {/* <Button title="Add Item" onPress={() => navigator.push("CreateProduct")} /> */}
-        <Button title="Logout" onPress={() => navigator.pop()} />
-    </View>
-);
+import BusinessHeader from '../components/headers/BusinessHeader'
+
+const BusinessLanding = ({ navigator }) => {
+    
+
+    //component
+    //header with logout button
+    //Our products Flatlist
+    //navbar
+    return (
+        <View style={styles.screen}>
+            <BusinessHeader navigator={navigator}/>
+            <Text>I am the Products page</Text>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        backgroundColor: "#383F38",
     }
 });
 
