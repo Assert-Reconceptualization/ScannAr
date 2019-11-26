0bd47ee4627434363ce8037d1d71efe1fd82794f
Jade <jadejdoucet@gmail.com>
2019-11-25 18:27:11 -0600

    added mock data with urls

---
+++ client/mock-data/products.js
@@ -0,0 +1,14 @@
+export default products = [
+    {
+        url: 'https://files.slack.com/files-pri/T02P3HQD6-FQR2FLSUR/image_from_ios.jpg',
+        name: 'blender-bottle',
+        price: 10,
+        description: 'Wow, a nice bottle'
+    },
+    {
+        url: 'https://files.slack.com/files-pri/T02P3HQD6-FR16GPZFG/image_from_ios.jpg',
+        name: 'Cabinet',
+        price: 1000,
+        description: 'This is a big blue cabinet'
+    },
+];
