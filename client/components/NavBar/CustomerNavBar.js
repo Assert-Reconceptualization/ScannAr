e1c42adf61a538770927fee5344e2f51bba18709
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-26 15:11:11 -0600

    lint ARNavBar BusinessNavbar CustomerNavBar BusinessList

--- client/components/NavBar/CustomerNavBar.js
+++ client/components/NavBar/CustomerNavBar.js
@@ -1,40 +1,48 @@
-import React from "react";
-import { Text, View, StyleSheet, Button, TouchableHighlight, TouchableOpacity } from "react-native";
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 const CustomerNavBar = ({ navigator }) => {
-return (
-    <View style={styles.bar}>
-        <TouchableOpacity onPress={() => navigator.push("AR")}>
-                <Text style={styles.buttonText}>AR</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigator.pop()}>
-                <Text style={styles.buttonText}>Logout</Text>
-        </TouchableOpacity>
+  // eslint-disable-next-line no-use-before-define
+  const { bar, buttonText } = styles;
+  return (
+    <View style={bar}>
+      <TouchableOpacity onPress={() => navigator.push('AR')}>
+        <Text style={buttonText}>AR</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigator.pop()}>
+        <Text style={buttonText}>Logout</Text>
+      </TouchableOpacity>
     </View>
-    )
+  );
 };
 
 const styles = StyleSheet.create({
-    bar: {
-        // flex:1,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        height: "8%",
-        width: "100%",
-        backgroundColor: 'lightblue'
-    },
-    button: {
-        borderRadius: 10,
-        marginBottom: 1,
-        width: 260,
-        alignItems: 'center',
-        backgroundColor: '#2196F3'
-    },
-    buttonText: {
-        textAlign: 'center',
-        padding: 20,
-        color: 'white'
-    }
+  bar: {
+    // flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: '8%',
+    width: '100%',
+    backgroundColor: 'lightblue',
+  },
+  button: {
+    borderRadius: 10,
+    marginBottom: 1,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white',
+  },
 });
 
 export default CustomerNavBar;
