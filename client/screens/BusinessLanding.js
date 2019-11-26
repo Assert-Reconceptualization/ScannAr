e876cd9949c3ca2abe47e01e5e640a46713f5553
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-26 12:58:08 -0600

    create navbar and add button

--- client/screens/BusinessLanding.js
+++ client/screens/BusinessLanding.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, Button } from "react-native";
 
 import BusinessHeader from '../components/headers/BusinessHeader';
 import BusinessList from '../components/productLists/BusinessList';
+import BusinessNavbar from "../components/NavBar/BusinessNavbar";
 
 const BusinessLanding = ({ navigator }) => {
     
@@ -17,20 +18,35 @@ const BusinessLanding = ({ navigator }) => {
         <View style={styles.title}>
           <BusinessList />
         </View>
+        <View style={styles.addButton}>
+            <Button title="BIG ADD BUTTON" />
+        </View>
+        <View style={styles.navbar}>
+            <BusinessNavbar navigator={navigator} />
+        </View>
       </View>
     );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        backgroundColor: "#383F38",
-    },
-    title: {
-        flex: 1,
-        marginTop: "5%",
-        marginLeft: "5%"
-    }
+  screen: {
+    flex: 1,
+    backgroundColor: "#383F38"
+  },
+  title: {
+    flex: 1,
+    marginTop: "5%",
+    marginLeft: "5%"
+  },
+  navbar: {
+    height: "8%",
+    width: "100%",
+    marginBottom: 0
+  },
+  addButton: {
+      marginBottom: 20,
+      marginLeft: '50%'
+  }
 });
 
 export default BusinessLanding;
