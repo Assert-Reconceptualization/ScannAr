85bb1ad13170171e12c8b3db54f43694e0187d5b
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-27 14:12:51 -0600

    format login screen

--- business/screens/SignInScreen.js
+++ business/screens/SignInScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Button
+} from 'react-native';
 
 export default function SignInScreen(props) {
   const handleSignUp = () => {
@@ -9,8 +15,13 @@ export default function SignInScreen(props) {
   const {
     container,
     titleContainer,
+    subTitleContainer,
+    buttonContainer,
     titleLeftText,
     titleRightText,
+    subTitle,
+    signInButton,
+    signInText,
   } = styles;
   return (
     <View style={container}>
@@ -18,8 +29,18 @@ export default function SignInScreen(props) {
         <Text style={titleLeftText}>Scann</Text>
         <Text style={titleRightText}>AR</Text>
       </View>
-      
-      <Button title="Sign Up" onPress={handleSignUp} />
+      <View style={subTitleContainer}>
+        <Text style={subTitle}>
+          Business
+        </Text>
+      </View>
+      <View style={buttonContainer}>
+        <TouchableOpacity style={signInButton}>
+          <Text style={signInText}>Sign In</Text>
+          <Text>Google Icon</Text>
+        </TouchableOpacity>
+        <Button title="Register" onPress={handleSignUp} />
+      </View>
     </View>
   );
 }
@@ -30,11 +51,16 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: '20%',
     backgroundColor: '#3B423C',
-    alignItems: 'center',
   },
   titleContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  subTitleContainer: {
+    flex: 2,
+    alignItems: 'center'
   },
   titleLeftText: {
     color: '#EFF6E0',
@@ -46,4 +72,26 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: 'bold',
   },
+  subTitle: {
+    color: '#AEC3B0',
+    fontSize: 40,
+  },
+  buttonContainer: {
+    flex: 3,
+    alignItems: 'center'
+  },
+  signInButton: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#AEC3B0',
+    width: 300,
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  signInText: {
+    fontSize: 30,
+    color: '#AEC3B0'
+  }
 });
