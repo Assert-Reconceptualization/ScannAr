e85e8e05bf614ec0afcef81fd58353ed0edaadef
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-29 22:07:54 -0600

    business can upload picture from phone

--- business/components/NewProductModal.js
+++ business/components/NewProductModal.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext } from 'react';
-import { Modal, Text, Button, StyleSheet, View } from 'react-native';
+import { Modal, Text, Button, StyleSheet, View, TextInput, Image } from 'react-native';
 import BusinessContext from "../applicationState/BusinessContext";
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 export default function NewProductModal(props){
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
   const [price, setPrice] = useState("");
   const context = useContext(BusinessContext);
 
@@ -15,12 +18,42 @@ export default function NewProductModal(props){
     props.navigation.navigate({routeName: 'Home'});
   }
 
-  const { container } = styles;
+  const handleCamera = async () => {
+    const permission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    let image = await ImagePicker.launchImageLibraryAsync({base64: true});
+
+    // uncomment when using real phone
+    // const permission = await Permissions.askAsync(Permissions.CAMERA);
+    // let image = await ImagePicker.launchCameraAsync();
+
+    if(!image.cancelled){
+      const file = image.base64
+      // upload image to firebase
+      fetch("https://us-central1-scannar-260417.cloudfunctions.net/storeImage", {
+        method: "POST",
+        body: JSON.stringify({
+          image: file
+        })
+      })
+        .then(res => res.json())
+        .then(result => {
+          setImageUrl(result.imageUrl);
+        })
+        .catch(err => {console.log("Try uploading again!")})
+    }
+  }
+
+  const { container, image, photoContainer } = styles;
 
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={ container }>
-        <Text>Testing out this modal</Text>
+        <Text>Create AR product!</Text>
+        <View style={photoContainer}>
+          
+          {imageUrl ? <Image style={image} source={{uri: imageUrl}} /> : <Button title="Take a Picture!" onPress={handleCamera} />}
+          <TextInput placeholder="Name"/>
+        </View>
         <Button title="Submit" onPress={handleSubmit} />
       </View>
     </Modal>
@@ -30,6 +63,15 @@ export default function NewProductModal(props){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 90,
+    width: 90,
+    borderRadius: 5
+  },
+  photoContainer: {
     alignItems: 'center',
     justifyContent: 'center'
   }
