dbfcdf2f50216f1e7e6bdb8fb1ed5ae4d8718037
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-12-06 12:27:38 -0600

    update eslint config

--- business/screens/AddScreen.js
+++ business/screens/AddScreen.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-use-before-define */
 import React, { useState, useContext } from 'react';
 import {
-  Modal,
-  Text,
   Button,
   StyleSheet,
   View,
@@ -12,16 +13,17 @@ import {
   ActivityIndicator,
   ScrollView,
 } from 'react-native';
-import BusinessContext from "../applicationState/BusinessContext";
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import BusinessContext from '../applicationState/BusinessContext';
 import serverConfig from '../serverConfig';
 import TagPicker from '../components/TagPicker';
+
 const server = serverConfig().url;
 
-export default function AddScreen(props){
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+export default function AddScreen(props) {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [imageUrl, setImageUrl] = useState(null);
   const [price, setPrice] = useState(null);
   const context = useContext(BusinessContext);
@@ -29,13 +31,12 @@ export default function AddScreen(props){
   const [currentTag, setCurrentTag] = useState('default');
 
   const handleSubmit = () => {
-
     // make request to server POST
     fetch(`${server}/products`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
         Authorization: context.accessToken,
       },
       body: JSON.stringify({
@@ -43,53 +44,53 @@ export default function AddScreen(props){
         description,
         price,
         imageUrl,
-        idBusiness: context.currentBusiness.id
-      })
+        idBusiness: context.currentBusiness.id,
+      }),
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((newProduct) => {
         // save product tag
-        if(currentTag.name !== 'default'){
+        if (currentTag.name !== 'default') {
           saveProductTags(newProduct.id, currentTag);
         }
         // refresh inventory
         fetch(`${server}/products?idBusiness=${context.currentBusiness.id}`, {
-          method: "GET",
+          method: 'GET',
           headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
           },
         })
-          .then(response => response.json())
-          .then(products => {
-            //update current inventory if there are products
-            if(products.data){
-              context.setCurrentInventory(products.data)
+          .then((response) => response.json())
+          .then((products) => {
+            // update current inventory if there are products
+            if (products.data) {
+              context.setCurrentInventory(products.data);
             }
             // go back to home screen
-            props.navigation.navigate({routeName: 'Home'});
-          })
+            props.navigation.navigate({ routeName: 'Home' });
+          });
       })
       .catch(() => {
-        console.log("something went wrong");
-      })
-  }
+        console.log('something went wrong');
+      });
+  };
 
   const saveProductTags = (productId, tagName) => {
     // grab tag id
-    const { id } = context.tags.filter(tag => tag.name === tagName)[0];
+    const { id } = context.tags.filter((tag) => tag.name === tagName)[0];
     console.log(productId, id);
     fetch(`${server}/productTags?idProduct=${productId}&idTag=${id}`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
         Authorization: context.accessToken,
-      }
+      },
     })
-      .catch((err) => console.log(err))
-  }
-  
+      .catch((err) => console.log(err));
+  };
+
   const handleCamera = async () => {
     setSpinner(true); // turn spinner on
     // get permission to use camera
@@ -99,99 +100,99 @@ export default function AddScreen(props){
 
     // uncomment when using real phone
     const permission = await Permissions.askAsync(Permissions.CAMERA);
-    let image = await ImagePicker.launchCameraAsync({base64: true});
+    const image = await ImagePicker.launchCameraAsync({ base64: true });
 
     // upload image to firebase if user doesnt cancel
-    if(!image.cancelled){
+    if (!image.cancelled) {
       // extract base64 image data
-      const file = image.base64
+      const file = image.base64;
       // make request to cloud function
-      fetch("https://us-central1-scannar-260417.cloudfunctions.net/storeImage", {
-        method: "POST",
+      fetch('https://us-central1-scannar-260417.cloudfunctions.net/storeImage', {
+        method: 'POST',
         body: JSON.stringify({
-          image: file
-        })
+          image: file,
+        }),
       })
-        .then(res => res.json())
-        .then(result => {
+        .then((res) => res.json())
+        .then((result) => {
           setImageUrl(result.imageUrl);
         })
         .then(() => setSpinner(false)) // turn spinner off
         // TODO - message user to try again
-        .catch(err => {console.log("Try uploading again!")})
+        .catch(() => { console.log('Try uploading again!'); });
     }
-  }
+  };
 
   const resetScreenState = () => {
-    setName("");
-    setDescription("");
+    setName('');
+    setDescription('');
     setImageUrl(null);
-    setPrice("");
-  }
+    setPrice('');
+  };
 
   const {
     container,
     image,
     photoContainer,
-    titleContainer,
-    formContainer,
     textInput,
     descriptionInput,
   } = styles;
 
-  let imageText = spinner ? <ActivityIndicator size="small" color="black" /> : <Button
-    title={"Take a Picture!"}
-    onPress={handleCamera}
-  />;
+  const imageText = spinner ? <ActivityIndicator size="small" color="black" /> : (
+      <Button
+          title={'Take a Picture!'}
+          onPress={handleCamera}
+      />
+    );
 
   return (
-    <ScrollView style={{flex: 1}}>
+    <ScrollView style={{ flex: 1 }}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={ container }>
-            <View style={photoContainer}>
+        <View style={container}>
+          <View style={photoContainer}>
               {imageUrl ? (
                 <Image
                   style={image}
-                  source={{uri: imageUrl}}
+                  source={{ uri: imageUrl }}
                 />
               ) : (
-                  imageText
-                  )}
+                imageText
+              )}
             </View>
-            <TextInput
+          <TextInput
               placeholder="Name"
               style={textInput}
               value={name}
-              onChangeText={text => setName(text)}
-            />
-            <TextInput
+              onChangeText={(text) => setName(text)}
+          />
+          <TextInput
               placeholder="Price"
               value={price}
               style={textInput}
               keyboardType="decimal-pad"
-              onChangeText={text => setPrice(text)}
-            />
-            <TextInput
+              onChangeText={(text) => setPrice(text)}
+          />
+          <TextInput
               placeholder="Description"
               style={descriptionInput}
               value={description}
-              multiline={true}
-              onChangeText={text => setDescription(text)}
-            />
-            <TagPicker
+              multiline
+              onChangeText={(text) => setDescription(text)}
+          />
+          <TagPicker
               currentTag={currentTag}
               setCurrentTag={setCurrentTag}
-            />
-            <Button
+          />
+          <Button
               title="Submit"
-              onPress={handleSubmit} 
-            />
-            <Button
+              onPress={handleSubmit}
+          />
+          <Button
               title="clear fields"
               onPress={resetScreenState}
               color="red"
-            />
-          </View>
+          />
+        </View>
       </TouchableWithoutFeedback>
     </ScrollView>
   );
@@ -200,27 +201,27 @@ export default function AddScreen(props){
 AddScreen.navigationOptions = {
   title: 'Add A Product',
   headerStyle: {
-    backgroundColor: "#505950"
+    backgroundColor: '#505950',
   },
-  headerTintColor: "white"
+  headerTintColor: 'white',
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   titleContainer: {
-    flex: 1
+    flex: 1,
   },
   formContainer: {
-    flex: 8
+    flex: 8,
   },
   image: {
     width: 114,
     height: 114,
-    borderRadius: 5
+    borderRadius: 5,
   },
   photoContainer: {
     alignItems: 'center',
@@ -233,22 +234,22 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   textInput: {
-    width: "70%",
+    width: '70%',
     borderWidth: 2,
-    borderColor: "black",
+    borderColor: 'black',
     borderRadius: 5,
     fontSize: 25,
     marginBottom: 20,
-    paddingLeft: 5
+    paddingLeft: 5,
   },
   descriptionInput: {
-    width: "70%",
+    width: '70%',
     borderWidth: 2,
     height: 200,
-    borderColor: "black",
+    borderColor: 'black',
     borderRadius: 5,
     fontSize: 25,
     marginBottom: 20,
-    paddingLeft: 5
-  }
+    paddingLeft: 5,
+  },
 });
