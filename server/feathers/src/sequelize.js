2aa588dacab0f09150e764ba8ffa4ca688858582
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-12-07 11:49:14 -0600

    remove stock tests, add /product route test

--- server/feathers/src/sequelize.js
+++ server/feathers/src/sequelize.js
@@ -1,33 +1,33 @@
 const Sequelize = require('sequelize');
 
 module.exports = (app) => {
-  const sequelize = new Sequelize('scannar', 'postgres', 'scannar', {
-    dialect: 'postgres',
-    // e.g. host: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
-    host: '/cloudsql/scannar-server-second:us-central1:scannar',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    dialectOptions: {
-      // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
-      // same as host string above
-      socketPath: '/cloudsql/scannar-server-second:us-central1:scannar',
-    },
-    logging: false,
-    operatorsAliases: false,
-  });
-  // const connectionString = app.get("postgres");
-  // const sequelize = new Sequelize(connectionString, {
-  //   dialect: "postgres",
-  //   database: "ScannAr",
-  //   logging: false,
-  //   define: {
-  //     freezeTableName: true, // Model tableName will be the same as the model name
+  // const sequelize = new Sequelize('scannar', 'postgres', 'scannar', {
+  //   dialect: 'postgres',
+  //   // e.g. host: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+  //   host: '/cloudsql/scannar-server-second:us-central1:scannar',
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     acquire: 30000,
+  //     idle: 10000,
   //   },
+  //   dialectOptions: {
+  //     // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+  //     // same as host string above
+  //     socketPath: '/cloudsql/scannar-server-second:us-central1:scannar',
+  //   },
+  //   logging: false,
+  //   operatorsAliases: false,
   // });
+  const connectionString = app.get("postgres");
+  const sequelize = new Sequelize(connectionString, {
+    dialect: "postgres",
+    database: "ScannAr",
+    logging: false,
+    define: {
+      freezeTableName: true, // Model tableName will be the same as the model name
+    },
+  });
   const oldSetup = app.setup;
 
   app.set('sequelizeClient', sequelize);
@@ -45,28 +45,29 @@ module.exports = (app) => {
     app.set(
       'sequelizeSync',
       sequelize
-        .sync({ force: true })
-        .then(() => {
-          console.log('connected to database');
-          models.tags.create({
-            name: 'Real Estate',
-          });
-          models.tags.create({
-            name: 'Art',
-          });
-          models.tags.create({
-            name: 'Technology',
-          });
-          models.tags.create({
-            name: 'Furniture',
-          });
-          models.tags.create({
-            name: 'Collectables',
-          });
-          models.tags.create({
-            name: 'Miscellaneous',
-          });
-        })
+        .sync()
+        // create starter tags
+        // .then(() => {
+        //   console.log('connected to database');
+        //   models.tags.create({
+        //     name: 'Real Estate',
+        //   });
+        //   models.tags.create({
+        //     name: 'Art',
+        //   });
+        //   models.tags.create({
+        //     name: 'Technology',
+        //   });
+        //   models.tags.create({
+        //     name: 'Furniture',
+        //   });
+        //   models.tags.create({
+        //     name: 'Collectables',
+        //   });
+        //   models.tags.create({
+        //     name: 'Miscellaneous',
+        //   });
+        // })
         .catch((err) => {
           console.log(`Error: ${err}`);
         }),
--- server/feathers/test/app.test.js
+++ server/feathers/test/app.test.js
@@ -1,10 +1,12 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 const assert = require('assert');
 const axios = require('axios');
 const url = require('url');
 const app = require('../src/app');
 
-const hostname= app.get('host') || 'localhost';
-const port = app.get('port') || 8998;
+const hostname = 'localhost';
+const port = 3030;
 const getUrl = pathname => url.format({
   protocol: 'http',
   hostname,
@@ -12,55 +14,33 @@ const getUrl = pathname => url.format({
   pathname
 });
 
-describe('Feathers application tests', () => {
+describe('Feathers API tests', function () {
   let server;
 
-  before(function(done) {
+  before(function (done) {
     server = app.listen(port, hostname);
-    server.once('listening', () => done());
+    server.once('listening', function () { done(); });
   });
 
-  after(function(done) {
+  after(function (done) {
     server.close(done);
   });
 
-  it('starts and shows the index page', async () => {
+  it('Server is running', async function () {
     const { data } = await axios.get(getUrl());
 
     assert.ok(data.indexOf('<html lang="en">') !== -1);
   });
 
-  describe('404', function() {
-    it('shows a 404 HTML page', async () => {
-      try {
-        await axios.get(getUrl('path/to/nowhere'), {
-          headers: {
-            'Accept': 'text/html'
-          }
-        });
-        assert.fail('should never get here');
-      } catch (error) {
-        const { response } = error;
-
-        assert.equal(response.status, 404);
-        assert.ok(response.data.indexOf('<html>') !== -1);
-      }
+  describe('Products endpoint works', function () {
+    let products;
+    before(async function () {
+      const response = await axios.get(getUrl('/products'));
+      products = response.data.data;
     });
 
-    it('shows a 404 JSON error without stack trace', async () => {
-      try {
-        await axios.get(getUrl('path/to/nowhere'), {
-          json: true
-        });
-        assert.fail('should never get here');
-      } catch (error) {
-        const { response } = error;
-
-        assert.equal(response.status, 404);
-        assert.equal(response.data.code, 404);
-        assert.equal(response.data.message, 'Page not found');
-        assert.equal(response.data.name, 'NotFound');
-      }
+    it('/products returns an array', function () {
+      assert.ok(Array.isArray(products));
     });
   });
 });
